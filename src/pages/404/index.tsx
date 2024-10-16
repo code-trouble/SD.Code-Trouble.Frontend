034b5b1b5c7c85f78cd602930231ca1c89378b59
Fredaobob <lokgamer01@gmail.com>
2024-10-16 16:50:45 -0300

    feat: made and styled the error-page

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,3 +1,17 @@
 export const ErrorPage = () => {
-    return <h1>Error Page</h1>
+  const codeBrackets = '{code}'
+
+  return (
+    <div className="main-wrapper">
+      <div className="navbar">
+        <p>{codeBrackets} trouble </p>
+        <p>Suporte de Domingo</p>
+      </div>
+      <div className="hero">
+        <h1> 404 <span className="hide-on-mobile">-</span> <span className="enable-on-mobile"><br /></span> Page not Found </h1>
+        <p>A página que está procurando não existe ou está temporariamente indisponível.</p>
+        <button>Voltar para a Home</button>
+      </div>
+    </div>
+  );
 }
