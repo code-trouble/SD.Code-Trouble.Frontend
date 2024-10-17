7b787d2408c5dbafb39c3404fc7bd83f0d6486c4
Fredaobob <lokgamer01@gmail.com>
2024-10-17 14:27:48 -0300

    chore: removing uncecessary imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import './styles/sass/main.scss';
-import ms_sans_serif from 'react95/dist/fonts/ms_sans_serif.woff2';
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
