4beb79577c30002285bc8cc0761cf58e3cf41e5a
Fredaobob <lokgamer01@gmail.com>
2024-10-17 14:27:18 -0300

    feat: added viteCOmpression with brotli to plugins area in order to improve performance

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+    react(),
+    viteCompression({ 
+      algorithm: 'brotliCompress',
+      disable: false,
+    
+    })
+  ],
 
   css: {
     preprocessorOptions: {
