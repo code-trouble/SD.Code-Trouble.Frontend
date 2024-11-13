b7960742c733ed80f5801a7ad88e238c8bd4b42b
ardszsantos <lokgamer01@gmail.com>
2024-11-13 13:03:33 -0300

    refactoring dumb class names

--- src/components/StackedCard/index.tsx
+++ src/components/StackedCard/index.tsx
@@ -1,68 +1,59 @@
 import React from "react";
-import profileAvatar from "../../assets/images/png/profileAvatar.png"
-import vectorStar from "../../assets/images/png/starVector.png"
-import upvote from "../../assets/images/png/upvote.png"
-import comments from "../../assets/images/png/comments.png"
-import arrowButton from "../../assets/images/png/arrowButton.png"
+import profileAvatar from "../../assets/images/png/profileAvatar.png";
+import vectorStar from "../../assets/images/png/starVector.png";
+import upvote from "../../assets/images/png/upvote.png";
+import comments from "../../assets/images/png/comments.png";
+import arrowButton from "../../assets/images/png/arrowButton.png";
 
 export const StackedCard: React.FC = () => {
-
   return (
-    <div className="card-wrapper">
-        <div className="upper-div">
-          <h1 className="upper-div-h1">Escolhas da comunidade</h1>
-        </div>
-        <div className="lower-div">
-          <div className="lower-div-top">
-            <div className="lower-div-top-left">
-              <img src={profileAvatar} alt="profileAvatar"/>
-              <h2 className="lower-div-top-h2">Joana Lima</h2>
-            </div>
-            <div className="lower-div-top-right">
-              <img src={vectorStar} alt="estrela de favorito"/>
-            </div>
+    <div className="card">
+      <div className="card-header">
+        <h1 className="card-title">Escolhas da comunidade</h1>
+      </div>
+      <div className="card-content">
+        <div className="card-author">
+          <div className="author-info">
+            <img src={profileAvatar} alt="profileAvatar" />
+            <h2 className="author-name">Joana Lima</h2>
           </div>
-          
-          <div className="lower-div-bottom">
-            <h1 className="lower-div-bottom-h1">
-              Porque Designers <span className="lower-div-bottom-h1-span">merecem mais respeito</span>
-            </h1>
-            <p className="lower-div-bottom-p">An unconventional and compassionate guide to becoming an early bird.</p>
-            <div className="lower-div-bottom-tags">
-              <button className="lower-div-bottom-tags-tag">
-                Chat GPT
-              </button>
-              <button className="lower-div-bottom-tags-tag">
-                Design
-              </button>
-              <button className="lower-div-bottom-tags-tag">
-                Auto-Ajuda
-              </button>
-              <button className="lower-div-bottom-tags-tag">
-                Pix
-              </button>
-            </div>
+          <div className="favorite-icon">
+            <img src={vectorStar} alt="estrela de favorito" />
+          </div>
+        </div>
 
-            <div className="lower-div-bottom-details">
-              <div className="lower-div-bottom-details-left">
-                <p className="lower-div-bottom-details-left-date">
-                  10 Nov, 2024
-                </p>
-                <p className="lower-div-bottom-details-left-upvote">
-                  <img src={upvote} alt=""/>
-                  120k
-                </p>
-                <p className="lower-div-bottom-details-left-comments">
-                  <img src={comments} alt=""/>
-                  302
-                </p>
-              </div>
-              <div className="lower-div-bottom-details-right">
-                <img src={arrowButton} alt=""/>
-              </div>
+        <div className="card-body">
+          <h1 className="card-heading">
+            Porque Designers <span className="highlight-text">merecem mais respeito</span>
+          </h1>
+          <p className="card-description">
+            An unconventional and compassionate guide to becoming an early bird.
+          </p>
+          <div className="tag-list">
+            <button className="tag-item">Chat GPT</button>
+            <button className="tag-item">Design</button>
+            <button className="tag-item">Auto-Ajuda</button>
+            <button className="tag-item">Pix</button>
+          </div>
+
+          <div className="card-details">
+            <div className="details-left">
+              <p className="post-date">10 Nov, 2024</p>
+              <p className="upvote-count">
+                <img src={upvote} alt="" />
+                120k
+              </p>
+              <p className="comment-count">
+                <img src={comments} alt="" />
+                302
+              </p>
+            </div>
+            <div className="details-right">
+              <img src={arrowButton} alt="" />
             </div>
           </div>
         </div>
+      </div>
     </div>
   );
 };
