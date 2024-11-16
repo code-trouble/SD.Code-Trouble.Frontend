5909a9618e186d6e1f7e269d83a21d074c11997a
ardszsantos <lokgamer01@gmail.com>
2024-11-16 17:37:26 -0300

    feat: removed the previous testing component and added the new one

--- src/pages/TestingPage/index.tsx
+++ src/pages/TestingPage/index.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { NavBar } from "../../components/Navbar";
-import { Avatar } from "../../components/Avatar";
+import { StackedCards } from "../../components/TeamStackedCard/StackedCards";
+
+
+
 
 export const TestingPage: React.FC = () => {
   return (
     <div className="main-wrapper">
       <NavBar />
       <div className="hero">
-        <Avatar name="Professor Corrêa" role="Product Design" sizes="large" />
-      </div>
+          <StackedCards />
+      </div>  
     </div>
   );
 };
