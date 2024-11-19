1c1af56b5d5dd0425e9c73fc6dfa141bb685c807
ardszsantos <lokgamer01@gmail.com>
2024-11-19 12:56:53 -0300

    chore: renamed the 1st navbar to oldNavbar

--- src/components/oldNavbar/index.tsx
+++ src/components/oldNavbar/index.tsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import React from "react";
 
-export const NavBar: React.FC = () => {
+export const OldNavbar: React.FC = () => {
   const codeBrackets = "{code}";
 
   return (
