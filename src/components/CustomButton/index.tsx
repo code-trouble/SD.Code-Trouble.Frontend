0353d330ea50e954ea67887b21ded71774d27f4e
ardszsantos <lokgamer01@gmail.com>
2024-11-21 17:14:26 -0300

    feat: started developing the customButton

---
+++ src/components/CustomButton/index.tsx
@@ -0,0 +1,37 @@
+import { ReactNode } from "react";
+
+interface ICustomButton {
+    text?: string;
+    padding?: string;
+    color?: string;
+    backgroundColor?: string;
+    fontSize?: string;    
+    fontWeight?: string;
+    icon?: string; 
+}
+
+
+export const CustomButton: React.FC<ICustomButton> = ({
+    text,
+    padding,
+    color,
+    backgroundColor,
+    fontSize, 
+    fontWeight,
+    icon
+}) => {
+    return (  
+        <button className="custom-button" style={{ 
+            padding,
+            color,
+            backgroundColor,
+            fontSize, 
+            fontWeight        
+            }}>
+            {text}
+            {icon && <img src={icon}/>}
+        </button>
+    );
+};
+
+export default CustomButton;
