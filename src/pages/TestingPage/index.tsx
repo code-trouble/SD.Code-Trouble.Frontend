a94ab7a3692b16792116436fac0b16f9f8deb776
ardszsantos <lokgamer01@gmail.com>
2024-11-22 13:59:20 -0300

    chore: put the stacked cards component in the testing site to test (obviously...)

--- src/pages/TestingPage/index.tsx
+++ src/pages/TestingPage/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { OldNavbar } from "../../components/previewNavbar";
-import CustomButton from "../../components/CustomButton";
-import buttonArrow from "../../assets/images/svg/buttonArrow.svg"
+import { StackedCards } from "../../components/StackedCards";
+
 
 export const TestingPage: React.FC = () => {
 
@@ -9,34 +9,7 @@ export const TestingPage: React.FC = () => {
     <div className="main-wrapper">
       <OldNavbar/>
       <div className="hero">
-        <CustomButton 
-        text="Cadastro"
-        padding="8px 90px"
-        color="white" 
-        backgroundColor="#2DBA4F"
-        fontSize="18px"
-        fontWeight="500"
-        />
-
-        <CustomButton 
-        text="Visitar a comunidade"
-        padding="9.5px"
-        color="#3348A4" 
-        backgroundColor="transparent"
-        fontSize="18px"
-        fontWeight="500"
-        icon={buttonArrow}
-        />
-
-        <CustomButton 
-        text="Visitar Blog"
-        padding="9.5px 80px"
-        color="white" 
-        backgroundColor="#3348A4"
-        fontSize="18px"
-        fontWeight="500"
-        />
-
+        <StackedCards/>
       </div>  
     </div>
   );
