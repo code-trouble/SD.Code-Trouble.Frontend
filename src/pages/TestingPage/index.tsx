ab171a5343c6095e5b91860dccea5adf97644c6b
ardszsantos <lokgamer01@gmail.com>
2024-12-08 11:14:11 -0300

    feat: imported the component to test

--- src/pages/TestingPage/index.tsx
+++ src/pages/TestingPage/index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { Header } from "../../components/Header";
+import { PostWriter } from "../../components/PostWriter";
+
+
+
 export const TestingPage: React.FC = () => {
 
   return (
     <div className="main-wrapper">
       <Header theme="blue" loggedIn={true}/>
+
       
+      <PostWriter/>
     </div>
   );
 };
